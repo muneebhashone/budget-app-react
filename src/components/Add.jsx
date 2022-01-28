34a02b9d5e911d634211b41a8d0709cb8511c2a5
unknown <muneeb.hashone@gmail.com>
2022-01-28 07:20:51 +0500

    Remove useCallback hook

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect, useCallback } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { useAppContext } from "../Context/AppContext";
 
 function Add() {
